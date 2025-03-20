89e35b911778d86c2f308c796832085c79a53ab5
Rasmus Lellep <rlellep@Rasmuss-MacBook-Pro.local>
2025-03-20 12:31:42 +0100

    updated version

--- lib/variables.dart
+++ lib/variables.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 const String bundleID = 'ee.ut.cs.nlp.neurokone';
 
 const String appTitle = 'TartuNLP Neurokõne';
-const String appVersion = 'v1.3';
+const String appVersion = 'v1.4';
 
 String initialLanguage = 'Eesti';
 
